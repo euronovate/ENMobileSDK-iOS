3422d6e13c994f298fed71bc8d987cf84bdafa25
Andrea Bellotto <andreabellotto88@gmail.com>
2022-03-29 12:24:55 +0200

    adding projects

---
+++ Examples/DSPubSubSoftServerSignature/DSPubSubSoftServerSignature/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  DSPubSubSoftServerSignature
+//
+//  Created by Andrea Bellotto on 22/03/22.
+//
+
+import UIKit
+import ENMobileSDK
+import ENDigitalSignage
+import ENSoftServer
+import ENViewer
+import ENPubSub
+import ENSignatureBox
+class ViewController: UIViewController {
+
+    var isFirstLoad: Bool = true
+
+    let baseUrl = ""
+    
+    let licenseKey = ""
+    var username = ""
+    var password = ""
+    
+    let logServerConfig = ENLogServerConfig(
+        baseURL: "",
+        licenseCode: "",
+        userID: ""
+    )
+    
+    let networkConfig = ENNetworkConfig(
+        skipSSL: true
+    )
+    
+    let digitalSignageConfig = ENDigitalSignageConfig(
+        baseURL: "",
+        licenseCode: ""
+    )
+    
+    let softServerConfig = ENSoftServerConfig(baseURL: "", licenseCode: "", userID: "")
+
+    let pubSubConfig = DefaultPubSubConfig(url: "", token: "", pubSubServiceType: .signalR, messagePath: ["", ""])
+
+    let certificateOwnerInfo = ENCertificateOwnerInfo(organization: "", countryCode: "", localityName: "", commonName: "")
+
+    let signatureBoxConfig = ENSignatureBoxConfig(signatureSourceType: .any, signatureImageConfig: .signatureAndTimestamp, useAlpha: true, signatureContentMode: .keepFieldRatio, enableSignatureOverwrite: true)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if isFirstLoad {
+            isFirstLoad = false
+            buildMobileSDK()
+        }
+        self.showLoader()
+    }
+    
+    func buildMobileSDK() {
+        SettingsManager.shared.resetSettingsManager()
+
+        Task {
+            let mobileSDK = await ENMobileSDK
+                .with(
+                    enAuthConfig: ENAuthConfig(baseUrl: baseUrl, license: licenseKey, username: username, password: password),
+                    enMobileSDKConfig:ENMobileSDKConfig(enabledLanguages: [.en, .gr], certificateSourceType: .generated, certificateOwnerInfo: .init(), networkConfig: networkConfig))
+                .with(customTheme: nil)
+                .with(certificateOwnerInfo: certificateOwnerInfo)
+                .with(logLevel: .verbose, logServerConfig: logServerConfig, saveLogsIniCloud: false)
+                .with { initCallback in
+                    switch initCallback {
+                    case .error(let error):
+                        ENMobileSDK.shared?.manageError(error, completion: {
+                            self.buildMobileSDK()
+                        })
+                    default:
+                        break
+                    }
+                    print(initCallback)
+                }
+                .with(responseCallback: { responseCallback in
+                    print(responseCallback)
+                })
+                .with(authenticable: ENSoftServer.with(softServerConfig: softServerConfig).build())
+                .with(authenticable: ENPubSub.with(pubsubConfig: pubSubConfig).build())
+                .with(authenticable: ENViewer.build())
+                .with(authenticable: ENSignatureBox.with(signatureBoxConfig: signatureBoxConfig).build())
+                .with(authenticableToEndBuilder:
+                        ENDigitalSignage.with(digitalSignageConfig: digitalSignageConfig)
+                        .build()
+                )
+                .build()
+            self.hideLoader()
+            mobileSDK.pubsub?.start()
+            mobileSDK.digitalSignage?.present(in: self)
+
+        }
+    }
+
+}
+
---
+++ Examples/DigitalSignage/DigitalSignage/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  DigitalSignage
+//
+//  Created by Andrea Bellotto on 22/03/22.
+//
+
+import UIKit
+import ENDigitalSignage
+import ENMobileSDK
+
+class ViewController: UIViewController {
+    
+    var isFirstLoad: Bool = true
+
+    let baseUrl = ""
+    
+    let licenseKey = ""
+    var username = ""
+    var password = ""
+    
+    let logServerConfig = ENLogServerConfig(
+        baseURL: "",
+        licenseCode: "",
+        userID: ""
+    )
+    
+    let mobileSDKConfig = ENMobileSDKConfig(enabledLanguages: [.en, .gr], certificateSourceType: .generated, certificateOwnerInfo: .init(), networkConfig: ENNetworkConfig(
+        skipSSL: true)
+    )
+    
+    let digitalSignageConfig = ENDigitalSignageConfig(
+        baseURL: "",
+        licenseCode: ""
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if isFirstLoad {
+            isFirstLoad = false
+            buildMobileSDK()
+        }
+        self.showLoader()
+    }
+    
+    func buildMobileSDK() {
+        SettingsManager.shared.resetSettingsManager()
+
+        Task {
+            let mobileSDK = await ENMobileSDK
+                .with(
+                    enAuthConfig: ENAuthConfig(baseUrl: baseUrl, license: licenseKey, username: username, password: password),
+                    enMobileSDKConfig: mobileSDKConfig)
+                .with(customTheme: nil)
+                .with(certificateOwnerInfo: ENCertificateOwnerInfo(organization: "Walt Disney", countryCode: "Italia", localityName: "Samarate", commonName: "Pluto"))
+                .with(logLevel: .verbose, logServerConfig: logServerConfig, saveLogsIniCloud: false)
+                .with { initCallback in
+                    switch initCallback {
+                    case .error(let error):
+                        ENMobileSDK.shared?.manageError(error, completion: {
+                            self.buildMobileSDK()
+                        })
+                    default:
+                        break
+                    }
+                    print(initCallback)
+                }
+                .with(responseCallback: { responseCallback in
+                    print(responseCallback)
+                })
+                .with(authenticableToEndBuilder:
+                        ENDigitalSignage.with(digitalSignageConfig: digitalSignageConfig)
+                        .build()
+                )
+                .build()
+            self.hideLoader()
+            mobileSDK.digitalSignage?.present(in: self)
+
+        }
+    }
+
+}
+
